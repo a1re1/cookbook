c8aac1b5a0f248d745a8b2a0c34a46f7bf535729
Tyler Whitehurst <twwhitehurst@gmail.com>
2023-12-23 03:15:13 +0000

    search

--- src/routes/new-recipe/+page.svelte
+++ src/routes/new-recipe/+page.svelte
@@ -1,4 +1,8 @@
 <script>
-    // redirect to recipe page with random id
-    window.location.replace("/recipe/" + Math.random().toString(36).substring(2, 15));
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        // redirect to recipe page with random id
+        window.location.replace("/recipe/" + Math.random().toString(36).substring(2, 15));
+    });
 </script>
